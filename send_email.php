70302213dd6275a23c0773fa15ab61c378f28a13
davitacols <98448386+davitacols@users.noreply.github.com>
2024-05-21 10:37:36 +0100

    Mailer

--- send_email.php
+++ send_email.php
@@ -2,49 +2,45 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
-
-// Load environment variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+require 'vendor/PHPMailer/src/Exception.php';
+require 'vendor/PHPMailer/src/PHPMailer.php';
+require 'vendor/PHPMailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    if (empty($name) || empty($email) || empty($message)) {
+    if (!empty($name) && !empty($email) && !empty($message)) {
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = $_ENV['GMAIL_USER'];
+            $mail->Password = $_ENV['GMAIL_PASS'];
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+
+            //Recipients
+            $mail->setFrom('your-email@gmail.com', 'Mailer');
+            $mail->addAddress('recipient-email@example.com', 'Recipient Name'); // Add a recipient
+
+            //Content
+            $mail->isHTML(true);
+            $mail->Subject = 'New Contact Form Submission';
+            $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } else {
         echo 'Please fill out all fields.';
-        exit;
     }
-
-    $mail = new PHPMailer(true);
-    try {
-        // Server settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = $_ENV['GMAIL_USER'];
-        $mail->Password = $_ENV['GMAIL_PASS'];
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
-
-        // Recipients
-        $mail->setFrom($_ENV['GMAIL_USER'], 'David Ansa');
-        $mail->addAddress('recipient_email@example.com', 'Recipient Name');
-
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = 'Contact Form Submission';
-        $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
-        $mail->AltBody = "Name: $name\nEmail: $email\nMessage: $message";
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-} else {
-    echo 'Invalid request method';
 }
 ?>
+
